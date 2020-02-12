873f5148189bfeff34fa7af3bfc39b41f41e53bd
kz <ma.kozeev@gmail.com>
2020-02-12 23:54:14 +0300

    some test were added

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,19 +3,25 @@ from pages.locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
+    def should_be_add_to_cart_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_TO_CART_BUTTON), 'No add to cart button'
+
     def add_to_cart(self):
-        self.name = self.browser.find_element(*ProductPageLocators.NAME).text
-        self.price = self.browser.find_element(*ProductPageLocators.PRICE).text
         btn = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
         btn.click()
-        self.solve_quiz_and_get_code()
-        self.name_should_be_equal_to_alert_name()
-        self.price_should_be_equal_to_basket_price()
 
     def name_should_be_equal_to_alert_name(self):
+        name = self.browser.find_element(*ProductPageLocators.NAME).text
         success_name_alert = self.browser.find_element(*ProductPageLocators.SUCCESS_NAME_ALERT).text
-        assert self.name == success_name_alert, 'Name in the alert is not equal to name'
+        assert name == success_name_alert, 'Name in the alert is not equal to name'
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_NAME_ALERT)
+
+    def success_message_should_disappear(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_NAME_ALERT), 'success alert doesnt disappear'
 
     def price_should_be_equal_to_basket_price(self):
+        price = self.browser.find_element(*ProductPageLocators.PRICE).text
         basket_amount_alert = self.browser.find_element(*ProductPageLocators.BASKET_AMOUNT_ALERT).text
-        assert self.price == basket_amount_alert, 'Price in the basket is not equal to price'
+        assert price == basket_amount_alert, 'Price in the basket is not equal to price'
--- test_product_page.py
+++ test_product_page.py
@@ -10,17 +10,44 @@ def test_guest_can_add_product_to_basket(browser):
     page.add_to_cart()
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+mask = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+xfile = 7
+params = [f'?promo=offer{i}' for i in range(10) if i != xfile]
+links = [f'{mask}/{param}' for param in params]
+xfile_link = pytest.param(f'{mask}/?promo=offer{xfile}',
+                          marks=pytest.mark.xfail(reason='Product name not equal to alert name'))
+links.append(xfile_link)
+
+
+@pytest.mark.parametrize('link', links)
 def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
+    page.should_be_add_to_cart_button()
     page.add_to_cart()
+    page.solve_quiz_and_get_code()
+    page.name_should_be_equal_to_alert_name()
+    page.price_should_be_equal_to_basket_price()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link, timeout=None)  # None because some methods use explict wait
+    page.open()
+    page.add_to_cart()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link, timeout=None)  # None because some methods use explict wait
+    page.open()
+    page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, link, timeout=None)  # None because some methods use explict wait
+    page.open()
+    page.add_to_cart()
+    page.success_message_should_disappear()
